b16d7bdc26871a5ba6b9e841d5257a5ce2f4daad
Ivan <ivankuartz@hotmail.com>
2022-08-03 17:50:18 -0300

    Add foreign key destino do lote to lote

---
+++ db/migrate/20220803202030_add_local_de_aplicacao_ref_to_lote.rb
@@ -0,0 +1,5 @@
+class AddLocalDeAplicacaoRefToLote < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :lotes, :destino_do_lote, null: false, foreign_key: {to_table: :local_de_aplicacaos}
+  end
+end
