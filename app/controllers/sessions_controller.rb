92580a65e4875a1d987a6fc6b11d04cb77d32e3a
Ivan <ivankuartz@hotmail.com>
2022-08-03 20:01:00 -0300

    Add sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+
+    def create
+        @user = User.find_by(username: params[:username])
+
+        if !!@user && @user.authenticate(params[:password])
+
+            session[:user_id] = @user.id
+            redirect_to user_path        
+       else
+
+        message = "Seu nome de usuário ou senha estão incorretos. Verifique-os, e tente novamente."
+        redirect_to login_path, notice: message
+       endereco_id
+       end
+    end
+    
+end
