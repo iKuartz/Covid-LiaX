2d9f9622abd19e27d0ac394f0bb390e3fa41823a
Ivan <ivankuartz@hotmail.com>
2022-08-03 17:48:38 -0300

    Add foreign key endereco to local de aplicacao

---
+++ db/migrate/20220803201655_add_localidade_ref_to_local_de_aplicacao.rb
@@ -0,0 +1,5 @@
+class AddLocalidadeRefToLocalDeAplicacao < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :local_de_aplicacaos, :endereco, null: false, foreign_key: {to_table: :localidades}
+  end
+end
