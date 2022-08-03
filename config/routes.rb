fcd1e32964b541c792954cb0811ccb21b01eef0c
Ivan <ivankuartz@hotmail.com>
2022-08-03 20:03:05 -0300

    Add routes for login

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'sessions#home'
+
+  resources :users, only: [:new, :create, :edit, :update, :show, :destroy]
+
+
+  get '/login', to: 'sessions#login'
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
+  get '/logout', to: 'sessions#destroy'
+  
 end
