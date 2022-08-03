7da4c0baabe17697fd1c7452fc427070fbc61bc6
Ivan <ivankuartz@hotmail.com>
2022-08-03 17:51:02 -0300

    Add foreign key detalhes da vacina to inventario

---
+++ db/migrate/20220803202533_add_detalhes_da_vacina_to_inventario.rb
@@ -0,0 +1,5 @@
+class AddDetalhesDaVacinaToInventario < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :inventarios, :Vacina, null: false, foreign_key: {to_table: :detalhes_da_vacinas}
+  end
+end
