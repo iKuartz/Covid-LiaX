6843c8233d6dab938526da0b70d031d2aa6f82c2
Ivan <ivankuartz@hotmail.com>
2022-08-03 17:49:11 -0300

    Add foreign key responsavel to local de aplicacao

---
+++ db/migrate/20220803201740_add_pessoa_ref_to_local_de_aplicacao.rb
@@ -0,0 +1,5 @@
+class AddPessoaRefToLocalDeAplicacao < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :local_de_aplicacaos, :responsavel, null: false, foreign_key: {to_table: :pessoas}
+  end
+end
