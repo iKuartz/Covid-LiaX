beb457eeb11366bf34e6e5ce5d6d1a474ecd55d7
Ivan <ivankuartz@hotmail.com>
2022-08-03 17:46:54 -0300

    Create restricoes table

--- db/migrate/20220803010329_create_restricaos.rb
+++ db/migrate/20220803010329_create_restricaos.rb
@@ -2,6 +2,13 @@
 
 class CreateRestricaos < ActiveRecord::Migration[7.0]
   def change
-    create_table :restricaos, &:timestamps
+    create_table :restricaos do |t|
+      t.integer :primeira_dose
+      t.integer :segunda_dose
+      t.integer :terceira_dose
+      t.integer :quarta_dose
+      
+      t.timestamps
+    end
   end
 end
