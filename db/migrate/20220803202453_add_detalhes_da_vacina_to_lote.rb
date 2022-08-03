65cc28056b11c1ecc46a3879970811a05ccb0026
Ivan <ivankuartz@hotmail.com>
2022-08-03 17:50:50 -0300

    Add foreign key detalhes da vacina to lote

---
+++ db/migrate/20220803202453_add_detalhes_da_vacina_to_lote.rb
@@ -0,0 +1,5 @@
+class AddDetalhesDaVacinaToLote < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :lotes, :vacina_do_lote, null: false, foreign_key: {to_table: :detalhes_da_vacinas}
+  end
+end
