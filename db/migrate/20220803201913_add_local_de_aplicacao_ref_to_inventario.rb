421912ea0010f9d5473cc2bc208e714483f761a4
Ivan <ivankuartz@hotmail.com>
2022-08-03 17:50:03 -0300

    Add foreign key local de aplicacao to inventário

---
+++ db/migrate/20220803201913_add_local_de_aplicacao_ref_to_inventario.rb
@@ -0,0 +1,5 @@
+class AddLocalDeAplicacaoRefToInventario < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :inventarios, :local_de_aplicacao, null: false, foreign_key: {to_table: :local_de_aplicacaos}
+  end
+end
