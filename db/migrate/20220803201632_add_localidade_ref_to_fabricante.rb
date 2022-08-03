42c6dc9c38488139f5c38638a5a8d378b1170f12
Ivan <ivankuartz@hotmail.com>
2022-08-03 17:48:12 -0300

    Add foreign key endereco

---
+++ db/migrate/20220803201632_add_localidade_ref_to_fabricante.rb
@@ -0,0 +1,5 @@
+class AddLocalidadeRefToFabricante < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :fabricantes, :endereco, null: false, foreign_key: {to_table: :localidades}
+  end
+end
