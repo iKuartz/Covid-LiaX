8d3ed2e55f985d3537f6296b9fd7591c7e34c013
Ivan <ivankuartz@hotmail.com>
2022-08-04 13:47:10 -0300

    Linter corrections

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,6 @@
 class SessionsController < ApplicationController
   def create
     @user = User.find_by(username: params[:username])
-    p !!@user && @user.authenticate(params[:password])
     if !!@user && @user.authenticate(params[:password])
 
       session[:user_id] = @user.id
