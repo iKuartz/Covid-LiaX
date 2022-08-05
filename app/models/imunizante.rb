c8047564a8fc1b317515b6f5dadfde43620197b2
Ivan <ivankuartz@hotmail.com>
2022-08-05 15:50:07 -0300

    Add foreign keys to imunizante model

--- app/models/imunizante.rb
+++ app/models/imunizante.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class Imunizante < ApplicationRecord
+    belongs_to :local_de_aplicacao
+    belongs_to :lote
+    belongs_to :pessoa, foreign_key: "paciente_id", class_name: "Pessoa"
 end
