34e73b64d2039f18adfc8644b5fc6831d74c9adf
Ivan <ivankuartz@hotmail.com>
2022-08-07 14:36:53 -0300

    Add Fabricantes and Locais to Dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -45,20 +45,15 @@ ActiveAdmin.register_page 'Dashboard' do
     end
     columns do
       column do
-        panel 'Pessoas' do
+        panel 'Fabricante' do
           ul class: 'tabela_dashboard' do
-            Pessoa.all.limit(4).map do |pessoa|
-              li "Nome: #{pessoa.nome}"
-              li "Email: #{pessoa.email}"
-              li "CPF: #{pessoa.cpf}"
-              li "Data de Nascimento: #{pessoa.data_nascimento}"
-              if pessoa.grupo_risco
-                li 'Pertence a grupo de risco?: Sim.'
-                li "Comorbidade: #{pessoa.comorbidade}"
-              else
-                li 'Pertence a grupo de risco?: Não.'
-              end
-              li "Telefone de contato: #{pessoa.telefone}"
+            Fabricante.all.limit(4).map do |fabricante|
+              li "Responsável: #{fabricante.pessoa.nome}"
+              li "Nome do fabricante: #{fabricante.nome}"
+              li "CNPJ: #{fabricante.cnpj}"
+              li "Telefone: #{fabricante.telefone}"
+              li "Email: #{fabricante.email}"
+              li "Endereço: #{fabricante.local.estado}, #{fabricante.local.cidade}, #{fabricante.local.bairro}, #{fabricante.local.rua}, #{fabricante.local.numero}, #{fabricante.local.complemento}"
               br
             end
           end
@@ -66,14 +61,20 @@ ActiveAdmin.register_page 'Dashboard' do
       end
 
       column do
-        panel 'Imunizantes' do
+        panel 'Locais de Aplicação' do
           ul class: 'tabela_dashboard' do
-            Imunizante.all.limit(3).map do |imunizante|
-              li "Local de aplicação: #{imunizante.local_de_aplicacao.nome_do_local}"
-              li "Paciente: #{imunizante.pessoa.nome}"
-              li "Lote: #{imunizante.lote.codigo}"
-              li "Data da aplicação: #{imunizante.data_de_aplicacao}"
-              li "Número da dose: #{imunizante.nro_da_dose}"
+            LocalDeAplicacao.all.limit(3).map do |local|
+              li "Nome do local: #{local.nome_do_local}"
+              li "Responsável: #{local.pessoa.nome}"
+              if local.ativo
+                li "Local ativo"
+              else
+                li "Local desativado"
+              end
+              li "CNPJ: #{local.cnpj}"
+              li "Telefone: #{local.telefone}"
+              li "Email: #{local.email}"
+              li "Endereço: #{local.local.estado}, #{local.local.cidade}, #{local.local.bairro}, #{local.local.rua}, #{local.local.numero}, #{local.local.complemento}"
               br
             end
           end
