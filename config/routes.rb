e1d6bec49745b2ae4b08b73d031a8334a3e6f1df
Ivan <ivankuartz@hotmail.com>
2022-08-07 17:39:04 -0300

    Add route to dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,5 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   post '/logout', to: 'sessions#destroy'
   get '/logout', to: 'sessions#destroy'
+  get '/dashboard', to: 'dashboards#index'
 end
