b5f62f2d19d6a92ff437465fe78af2a46985bb24
Ivan <ivankuartz@hotmail.com>
2022-08-07 13:00:17 -0300

    Create foreign keys in local de aplicação

--- app/models/local_de_aplicacao.rb
+++ app/models/local_de_aplicacao.rb
@@ -2,4 +2,6 @@
 
 class LocalDeAplicacao < ApplicationRecord
   has_many :imunizante
+  has_one :pessoa, foreign_key: 'responsavel_id', class_name: 'Pessoa'
+  has_one :local, foreign_key: 'endereco_id', class_name: 'Local'
 end
